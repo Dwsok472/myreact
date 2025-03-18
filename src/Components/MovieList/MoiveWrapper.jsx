21b0f3040bf8593551c40ac5676d3b976e49a932
sok472 <sok472@naver.com>
2025-03-18 13:40:42 +0900

    update

--- src/Components/MovieList/MoiveWrapper.jsx
+++ src/Components/MovieList/MoiveWrapper.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 function MoiveWrapper() {
   return (
     <div>
-      <h1>MovieWrapper입니다.</h1>
       <Outlet />
     </div>
   );
--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,11 +1,128 @@
-import React from 'react'
+import { categories, getGenreListMovie } from "./api";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Tab = styled.div`
+  display: flex;
+  margin: 10px 0;
+  gap: 8px;
+`;
+const Button = styled.button`
+  width: 130px;
+  height: 40px;
+  background-color: dodgerblue;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: #58d5ff;
+  }
+  &.selected {
+    background-color: #1a64b9;
+  }
+`;
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr; // ==repeat(3, 1fr); 밑에 열까지 확실하다면 row까지 줘도 됨
+  /* display: flex;
+  flex-wrap: wrap; */
+  gap: 10px;
+`;
+const Card = styled.div`
+  width: 100%;
+  /* width: 32%; */
+  border: 1px solid dodgerblue;
+  cursor: pointer;
+  padding: 10px;
+`;
+const Img = styled.img`
+  width: 100%;
+`;
+const Text = styled.div`
+  color: #333;
+  overflow-wrap: break-word;
+  word-break: break-all;
+`;
 
 function MovieList() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [selectedCat, setSelectedCat] = useState(0);
+  const IMG_PATH = "https://image.tmdb.org/t/p/original";
+
+  useEffect(() => {
+    getMovies(0);
+  }, []);
+
+  // 1. await는 반드시 async 함수 안에 사용한다.
+  // 2. try-catch 구문을 이용하는 것이 좋다.
+  async function getMovies(index) {
+    try {
+      const response = await categories[index].func(); // 200
+      console.log(response.data);
+      setSelectedCat(index);
+      setData(response.data);
+      setLoading(false);
+      getGenreListMovie();
+    } catch (error) {
+      // 400, 404, 500 기타 등등
+      console.log(error);
+    }
+  }
+
   return (
-  <div>
-    <h1>MovieList 입니다.</h1>
-  </div>
+    <div>
+      <h1>MovieList</h1>
+      <Tab>
+        {/* 1. 선택하면 해당 카테고리 무비리스트 요청(함수요청) 2. 카테고리에 맞게 버튼 활성화 */}
+        {categories.map((category, i) => (
+          <Button
+            key={i}
+            onClick={() => getMovies(i)}
+            className={i == selectedCat ? "selected" : ""}
+          >
+            {category.category}
+          </Button>
+        ))}
+      </Tab>
+      <Container>
+        {loading ? (
+          <p>로딩 중...</p>
+        ) : (
+          data.results.map((movie) => {
+            // 중괄호를 쓸거면 return이 필요하고
+            return (
+              <Card key={movie.id}>
+                <Img src={IMG_PATH + movie.poster_path}></Img>
+                <Text>타이틀 : {movie.title}</Text>
+                <Text>장르 : {movie.genre_ids}</Text>
+                {/* ① 컴포넌트 처음 로드할 때(==useEffect() []), 장르 리스트 요청 
+                    ② 장르 리스트 저장 
+                    ③ 변환함수 작성 -> 함수(숫자) return 장르텍스트 */}
+                <hr />
+                <Text>{movie.overview}</Text>
+              </Card>
+            );
+          })
+          // return 없이 쓰고싶다면 소괄호를 써야함.
+          // (
+          // <Card key={movie.id}>
+          //   <Img src=" "></Img>
+          //   <Text>타이틀 : {movie.title}</Text>
+          //   <Text>장르 : {movie.genre_ids}</Text>
+          //   <hr />
+          //   <Text>{movie.overview}</Text>
+          // </Card>
+          // ))
+        )}
+      </Container>
+    </div>
   );
 }
 
-export default MovieList
+export default MovieList;
